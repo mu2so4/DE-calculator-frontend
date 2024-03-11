c56f3cce983f8f5c83fc7b1fde0ee0a962e79792
mu2so4 <b4.orangutang@gmail.com>
2024-03-11 11:42:29 +0700

    Add digit and operators buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,9 +4,28 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+
+
 export default function Calculator() {
   const[expression,setExpression]=useState('');
   const[exprResult,setExprResult]=useState('');
+  const appendExpression=(str) => {
+    setExpression(expression + str)
+  }
+
+  const AC_ACTION = 'AC'
+
+  const handleClick=(value) => {
+    if(Number.isInteger(value) || value === '.') {
+      appendExpression(value)
+    }
+    else if(value === AC_ACTION) {
+      setExpression('')
+    }
+    else {
+      appendExpression(' ' + value + ' ')
+    }
+  }
 
   const handleSubmit=(e) => {
     e.preventDefault()
@@ -36,21 +55,60 @@ export default function Calculator() {
   }
 
   return (
-  <Box
-    component="form"
-    sx={{
-      '& > :not(style)': { m: 1, width: '25ch' },
-    }}
-    noValidate
-    autoComplete="off"
-  >
-    <form noValidate autoComplete="off">
-      <TextField id="expressionField" label="Expression" variant="outlined" value={expression}
-          onChange={(e) => setExpression(e.target.value)}/>
-      <Button id="evaluateButton" variant="contained" onClick={handleSubmit}>=</Button>
-    </form>
+    <Box
+      //component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '40ch' },
+      }}
+      //noValidate
+      //autoComplete="off"
+    >
 
-    <TextField id="resultField" variant="outlined" value={exprResult}/>
-  </Box>
+      <form noValidate autoComplete="off">
+        <TextField id="expressionField" variant="outlined" value={expression}/>
+      </form>
+
+      <Box>
+        <Button id="leftBracketButton" variant="outlined" onClick={() => handleClick('(')}>(</Button>
+        <Button id="rightBracketButton" variant="outlined" onClick={() => handleClick(')')}>)</Button>
+        <Button id="digit7Button" variant="outlined" onClick={() => handleClick('*')}>^</Button>
+        <Button id="pointButton" variant="outlined" onClick={() => handleClick(AC_ACTION)}>AC</Button>
+      </Box>
+      
+      <Box>
+        <Button id="digit7Button" variant="contained" onClick={() => handleClick(7)}>7</Button>
+        <Button id="digit8Button" variant="contained" onClick={() => handleClick(8)}>8</Button>
+        <Button id="digit9Button" variant="contained" onClick={() => handleClick(9)}>9</Button>
+        <Button id="pointButton" variant="outlined" onClick={() => handleClick('/')}>/</Button>
+      </Box>
+
+      <Box>
+        <Button id="digit4Button" variant="contained" onClick={() => handleClick(4)}>4</Button>
+        <Button id="digit5Button" variant="contained" onClick={() => handleClick(5)}>5</Button>
+        <Button id="digit6Button" variant="contained" onClick={() => handleClick(6)}>6</Button>
+        <Button id="pointButton" variant="outlined" onClick={() => handleClick('*')}>*</Button>
+      </Box>
+
+      <Box>
+        <Button id="digit1Button" variant="contained" onClick={() => handleClick(1)}>1</Button>
+        <Button id="digit2Button" variant="contained" onClick={() => handleClick(2)}>2</Button>
+        <Button id="digit3Button" variant="contained" onClick={() => handleClick(3)}>3</Button>
+        <Button id="pointButton" variant="outlined" onClick={() => handleClick('-')}>-</Button>
+      </Box>
+
+      <Box>
+        <Button id="digit0Button" variant="contained" onClick={() => handleClick(0)}>0</Button>
+        <Button id="pointButton" variant="outlined" onClick={() => handleClick('.')}>.</Button>
+        <Button id="evaluateButton" variant="contained" onClick={handleSubmit}>=</Button>
+        <Button id="pointButton" variant="outlined" onClick={() => handleClick('+')}>+</Button>
+      </Box>
+
+      
+      
+      
+      
+
+      <TextField id="resultField" variant="outlined" value={exprResult} />
+    </Box>
   );
 }
